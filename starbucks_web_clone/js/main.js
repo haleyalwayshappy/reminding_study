3002368147d7bb1f9c74ac4a618f59b3c5a4cd50
haleyalwayshappy <hongniest@gmail.com>
2021-05-21 02:42:20 +0900

    starbucks start

--- starbucks_web_clone/js/main.js
+++ starbucks_web_clone/js/main.js
@@ -1,15 +1,46 @@
-
-
-const searchEl = document.querySelector('.search');  
+const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function (){
 
+// 검색 바 호버 했을때!
+searchEl.addEventListener('click', function () {
   searchInputEl.focus();
-  
 });
 
-searchInputEl.addEventListener('focus',function(){
-  searchEl.classList.add();
-
+searchInputEl.addEventListener('focus', function () {
+  searchEl.classList.add('focused');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 });
+
+searchInputEl.addEventListener('blur', function () {
+  searchEl.classList.remove('focused');
+  searchInputEl.setAttribute('placeholder', ' ');
+});
+
+//badge 사라지게 하는거!
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log(window.scrollY);
+  if (window.scrollY > 500) {
+    //배지 숨기기
+    // 기본 js -> badgeEl.style.display='none';
+    // gsap.to(요소,지속시간,옵션 );
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+
+    });
+
+  } else {
+    //배지 보여주기
+    // 기본 js -> badgeEl.style.display='block';
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display:'block'
+
+    });
+  }
+}, 300));
+// _.throttle(함수, 시간)
