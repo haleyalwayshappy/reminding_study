3a90d0f35b95e0a671e6a11d63f3f8a5e3934a35
haleyalwayshappy <hongniest@gmail.com>
2021-06-08 04:35:36 +0900

    scss 공부

--- pages_real/js/common.js
+++ pages_real/js/common.js
@@ -1,1 +1,17 @@
 /** Header & footer*/
+
+// gulpfile.js
+var gulp = require('gulp')
+var sass = require('gulp-sass')
+
+// 일반 컴파일
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/*.scss')  // 입력 경로
+    .pipe(sass().on('errgor', sass.logError))
+    .pipe(gulp.dest('./dist/css'));  // 출력 경로
+});
+
+// 런타임 중 파일 감시
+gulp.task('sass:watch', function () {
+  gulp.watch('./src/scss/*.scss', ['sass']);  // 입력 경로와 파일 변경 감지 시 실행할 Actions(Task Name)
+});
