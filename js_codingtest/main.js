a8884b905379cab9d07f1e0257598af5629a353c
haleyalwayshappy <hongniest@gmail.com>
2021-07-26 01:07:56 +0900

    latest

---
+++ js_codingtest/main.js
@@ -0,0 +1,20 @@
+// *1번 문제
+
+var nums = [100, 200, 300, 400, 500];
+
+
+nums.pop();
+nums.pop();
+
+// console.log(nums);
+
+
+
+
+//  * 2번문제
+
+var arr = [200, 100, 300];
+// pass
+
+arr.splice(2,0,10000);
+console.log(arr);
